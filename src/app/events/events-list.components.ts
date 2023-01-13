1908086cccc44cf7efa4cd3ac9cd8a82c6670053
peti-krl <petia.dimkina@outlook.com>
2023-01-13 23:30:35 +0200

    templete variables #thumbnail to interact with child components

--- src/app/events/events-list.components.ts
+++ src/app/events/events-list.components.ts
@@ -6,8 +6,10 @@ import { Component } from "@angular/core";
     <div>
         <h1>Upcoming Angular Events</h1>
         <hr/>  
-        <event-thumbnail (eventClick)="handleEventClicked($event)" [event] = "event1"></event-thumbnail>
-    </div>   `
+        <event-thumbnail #thumbnail [event]="event1"></event-thumbnail>
+        <h3>{{thumbnail.someProperty}}</h3>
+        <button class="btn btn-primary" (click)="thumbnail.logFoo()">Log me some foo</button>  
+      </div>   `
 })
 
 export class EventsListComponent{
@@ -23,9 +25,5 @@ export class EventsListComponent{
             city: 'London',
             country: 'UK'
         }
-    }
-
-    handleEventClicked(data: any){
-        console.log('recieved: ',data)
     }
 }
