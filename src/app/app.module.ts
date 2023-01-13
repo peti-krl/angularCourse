a21078437056872a785df246486043078eca2b52
peti-krl <petia.dimkina@outlook.com>
2023-01-13 21:38:51 +0200

    communicating with Child component (@Input())

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.components';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/events/events-list.components.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'events-list',
+    template: `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr/>  
+        <event-thumbnail [event] = "event1"></event-thumbnail>
+    </div>   `
+})
+
+export class EventsListComponent{
+    event1 = {
+        id: 1,
+        name: 'First Angular Event',
+        date: '1/13/2023',
+        time: '15:44 pm',
+        price: 255.55,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+            address: 'John Smith Str',
+            city: 'London',
+            country: 'UK'
+        }
+    }
+}
