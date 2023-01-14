9e42e0c04e2ae24ae38cc62a80e25c14dd59ce3c
peti-krl <petia.dimkina@outlook.com>
2023-01-14 12:00:18 +0200

    ngFor and handling null or undefined values

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,25 @@ import { Component, Input } from "@angular/core";
     selector: 'event-thumbnail',
     template: `
     <div class = "well hoverwell thumbnail">
-    <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+    <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
         <div>
-            <span>Location: {{event.location.address}}</span>
+            <span>Location: {{event?.location.address}}</span>
             <span>&nbsp;</span>
-            <span>{{event.location.city}}, {{event.location.country}}</span>
+            <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div> 
+        <div>
+            Online Url: {{event?.location?.onlineUrl}}
+        </div>
     </div>
-    ` 
+    `,
+    styles: [`
+        .thumbnail {min-height: 201px;}
+        .pad-left {margin-left: 10px;}
+        .well div {color: #bbb}
+   ` ] 
 })
 
 export class EventThumbnailComponent{
