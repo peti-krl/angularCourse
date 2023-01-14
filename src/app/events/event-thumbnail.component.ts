3518ae1f552cacd5fa05eb88d2bfea73eb8b9200
peti-krl <petia.dimkina@outlook.com>
2023-01-14 22:14:31 +0200

    ngClass and ngSwitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,12 +6,16 @@ import { Component, Input } from "@angular/core";
     <div class = "well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [ngClass] = "getStartedClass()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'"> (Early Start) </span>
+            <span *ngSwitchCase="'10:00 am'"> (Late Start) </span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
         <div>Price: \${{event?.price}}</div>
         <div *ngIf="event?.location">
             <span>Location: {{event?.location.address}}</span>
-            <span>&nbsp;</span>
-            <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div> 
         <div *ngIf="event?.onlineUrl">
             Online Url: {{event?.onlineUrl}}
@@ -19,6 +23,8 @@ import { Component, Input } from "@angular/core";
     </div>
     `,
     styles: [`
+        .green {color: #003300 !important;}
+        .bold {font-weight: bold;}
         .thumbnail {min-height: 250px;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb}
@@ -27,4 +33,10 @@ import { Component, Input } from "@angular/core";
 
 export class EventThumbnailComponent{
     @Input() event:any
+
+    getStartedClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
 }
